fff92e37d719564f2379121d5b1556820365f830
Marina <marinasoon@gmail.com>
2024-05-23 18:49:24 +0300

    Initial commit

---
+++ src/test/java/ru/netology/loginPage.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+import ru.netology.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class loginPage {
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $("[data-test-id='login'] input").setValue(info.getLogin());
+        $("[data-test-id='password'] input").setValue(info.getPassword());
+        $("[data-test-id='action-login']").click();
+        return new VerificationPage();
+    }
+}
